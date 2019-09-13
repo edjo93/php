7ea453ad2ce89565a7d15ae1b85c4f8a5b87df65
edjo93 <eddas.carrasco@unitec.edu>
2019-09-12 23:07:13 -0600

    bloques php - separamos el codigo de las funciones

--- index.php
+++ index.php
@@ -5,12 +5,22 @@
     <title>no title</title>
 </head>
 <body>
+
 <?php
-    echo "primer mensaje <br>";
+    
 
     function ambito(){
         echo"segundo mensaje<br>";
     }
+    
+
+    
+?>
+
+<?php
+    echo "primer mensaje <br>";
+
+    
     ambito();
     echo"tercer mensaje<br>";
 
